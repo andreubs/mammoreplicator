99b741074b00529f822698f33618ac2b700a13cb
Andreu <andreubs@users.noreply.github.com>
2015-07-30 12:16:46 -0400

    Output parameters into PLY file
    Writing the code input parameters into the header section of the PLY file.
    Other minor modifications such as writing the conversion date (now "import time" required).

--- mammoreplicator.py
+++ mammoreplicator.py
@@ -10,6 +10,7 @@
 
 import numpy
 from numpy import *
+import time
 
 #   Instal DICOM reader for Python: http://www.pydicom.org/ (just run ez_setup.py as root)
 import dicom
@@ -29,19 +30,20 @@ compression_thickness    = 6.7    # cm
 average_breast_intensity = 750
 air_intensity            = 16383     # Pixel value outside the breast (air attenuation only)      Matthew Clark 
 
-output_binning  = 4               # Rebin the pixels to reduce the print resolution (1=no-binning, 2=average_4pixels...)
-subtract_layer = 1.5 #3.3
+output_binning = 8               # Rebin the pixels to reduce the print resolution (1=no-binning, 2=average_4pixels...)
+subtract_layer = 0.0 # cm
 output_dcm = 0     # 1==true, 0==false->do not output dicom file with thickness
 output_mm  = 1     # 1==true, 0==false->output triangles in cm
+output_base= 1     # 1==true, 0==false->add a flat support at the base of the object to define a solid object, or output just an empty 2D shell
 
 rows       = [0,2294] 
 columns    = [0,1914] 
 
 pixel_size      = 0.00941 # cm     # Desired pixel size in the printed phantom
 mfp_breast      = 1.284   # cm     # 16.5keV=1.284cm, 20keV=1.933cm;      // Molybdenum target, 28 kVp -> average energy 16.5 keV
-mfp_plastic     = 1.0     # cm  --> wild estimation assuming some combination of PMMA and TiO2 in the stereolithogaphy substrate                !!STL!!   
-    #mfp_plastic     = 0.863  # cm     # 16.5keV=0.863cm, 20keV=1.315cm
-
+mfp_plastic     = 1.0     # cm
+  #mfp_plastic = (1.0/21.47) # cm  <-- Aluminum, 15 keV; attenuation coeff. http://physics.nist.gov/PhysRefData/XrayMassCoef/ElemTab/z13.html (density=2.699g/cm3)
+  #mfp_plastic = 0.863  # cm     # 16.5keV=0.863cm, 20keV=1.315cm
 
 num_rows   = int((rows[1]-rows[0])/output_binning)*output_binning         # Processed sizwe will be a multiple of binning value 
 num_columns= int((columns[1]-columns[0])/output_binning)*output_binning
@@ -55,12 +57,14 @@ source_coord    = array([0.0, rows[1]*0.5*pixel_size, 66.0])  # Location x-ray s
 air_threshold   = 0   #!!DeBuG!! Currently not removing air pixels: drawing 0 height columns instead       # Any pixel below this threshold will be considered air and asigned 0 thickness    
 
 
+print "\n -- Input conversion parameters ("+time.strftime("%c")+"):"
 print "      compression_thickness    =", compression_thickness
 print "      average_breast_intensity =", average_breast_intensity
+print "      air_intensity            =", air_intensity
 print "      columns=["+str(columns[0])+","+str(columns[1])+"], num_columns = ", num_columns
 print "         rows=["+str(rows[0])+","+str(rows[1])+"], num_rows = ", num_rows
 #print "      max_pixel_value =", max_pixel_value
-print "      air_threshold   =", air_threshold
+#print "      air_threshold   =", air_threshold
 print "      mfp_breast      =", mfp_breast
 print "      mfp_plastic     =", mfp_plastic
 print "      center_pixel    =", center_pixel
@@ -69,11 +73,13 @@ print "      source_coord    =", source_coord
 print "      pixel_size      =", pixel_size
 print "      subtract_layer  =", subtract_layer
 print "      output_binning  =", output_binning
+print "      output_mm       =", output_mm
+print "      output_base     =", output_base
 print "      binned columns  = "+str(num_columns/output_binning)+", binned rows = "+str(num_rows/output_binning)
 print " "
 
 # Read image data:
-print ' Reading input file: ',image_file_name,'...'
+print ' -- Reading input file: ',image_file_name,'...'
 mammo = dicom.read_file(image_file_name) 
 
 
@@ -107,11 +113,13 @@ for j in range(rows[0], rows[1], output_binning):
     else:  
 
       if (mammo.pixel_array[j][i]>1):
-	  
-	  
-	    pix[j][i] = conversion_factor * log(float(mammo.pixel_array[j][i])/float(air_intensity))    #Inverted operation matt clark
-        # Test 1:   pix[j][i] = conversion_factor/log(float(mammo.pixel_array[j][i]))    #Inverted operation matt clark
-		# Original: pix[j][i] = log(float(mammo.pixel_array[j][i])) * conversion_factor  
+
+        # Conversion for raw mammograms "for processing":
+        pix[j][i] = conversion_factor * log(float(mammo.pixel_array[j][i])/float(air_intensity))    # Inverted operation: assuming air value = incident intensity (Matt Clark)
+        
+        # Conversion for post-processed mammograms "for presentation": 
+        #pix[j][i] = log(float(mammo.pixel_array[j][i])) * conversion_factor  
+        
       else:
         pix[j][i] = 0
 
@@ -127,35 +135,56 @@ if (output_dcm==1):
   mammo.PixelData = mammo.pixel_array.tostring()   # Save the data array into the actual pixel data string
   #dicom.write_file(image_file_name+"_plastic_bin"+str(output_binning)+".dcm", mammo)
   
-  dicom.write_file(image_file_name+"_STL_bin"+str(output_binning)+".dcm", mammo)       # !!STL!!
+  dicom.write_file(image_file_name+"_bin"+str(output_binning)+".dcm", mammo)
 
 
 # Generate triangle mesh:
 print '\n Generating the output triangle mesh focused to the x-ray source focal spot (num_air_pix='+str(num_air_pix)+')...'
 num_binned_pixels = (num_columns/output_binning) * (num_rows/output_binning) - num_air_pix
 num_vertices  = num_binned_pixels * 8     #  8 vertices per printed column  
-num_triangles = num_binned_pixels * 8 + 2*(num_rows/output_binning) + 2*(num_columns/output_binning)        #!!ReducedTriangles!!
 
-print ' Writing ',num_triangles,' triangles in output file: '+image_file_name+"_bin"+str(output_binning)+'.ply'
+if (output_base==1):
+  num_triangles = num_binned_pixels * 8 + 2*(num_rows/output_binning) + 2*(num_columns/output_binning)        #!!ReducedTriangles!!
+else:    
+  num_triangles = num_binned_pixels * 6 + 2*(num_rows/output_binning) + 2*(num_columns/output_binning)
 
-
-image_file_name2 = image_file_name+"_STL_bin"+str(output_binning)+"_focusedZ"+str(source_coord[2])      # !!STL!!
+  
+image_file_name2 = image_file_name+"_RawData_bin"+str(output_binning)+"_focusedZ"+str(source_coord[2])
 if (subtract_layer>0.00001):
   image_file_name2 = image_file_name2+"_-"+str(subtract_layer)+"cm"
 if (output_mm==1):
   image_file_name2 = image_file_name2+"_mm"
-image_file_name2 = image_file_name2+"_inverseNorm2.ply" #Matt clark inverse
+image_file_name2 = image_file_name2+".ply"
+
+print '\n -- Writing ',num_triangles,' triangles to output file: '+image_file_name2+'\n'
 
 ply = open(image_file_name2, 'w')  # Create PLY file and write header:
 
 ply.write('ply\n')
 ply.write('format ascii 1.0\n')
 ply.write('comment ** Geometry created by mammoreplicator.py using the input file: '+image_file_name+'\n')
-ply.write('comment    [Andreu Badal, 2015-02-03]\n')
+ply.write('comment      [Andreu Badal, 2015-02-03]\n')
 if (subtract_layer>0.00001):
   ply.write('comment NOTE: subtracting '+str(subtract_layer)+' cm from each column for easier printing.\n')
 if (output_mm==1):
   ply.write('comment NOTE: vertex coordinates in mm.\n')
+  
+ply.write('comment    -- Input conversion parameters ('+time.strftime("%c")+')\n')
+ply.write('comment          compression_thickness    = ' +str(compression_thickness)+'\n')
+ply.write('comment          average_breast_intensity = ' +str(average_breast_intensity)+'\n')
+ply.write('comment          air_intensity  = ' +str(air_intensity)+'\n')
+ply.write('comment          mfp_breast     = ' +str(mfp_breast)+'\n')
+ply.write('comment          mfp_plastic    = ' +str(mfp_plastic)+'\n')
+ply.write('comment          center_pixel   = ' +str(center_pixel)+'\n')
+ply.write('comment          center_coord   = ' +str(center_coord)+'\n')
+ply.write('comment          source_coord   = ' +str(source_coord)+'\n')
+ply.write('comment          pixel_size     = ' +str(pixel_size)+'\n')
+ply.write('comment          subtract_layer = ' +str(subtract_layer)+'\n')
+ply.write('comment          output_binning = ' +str(output_binning)+'\n')
+ply.write('comment          output_mm      = ' +str(output_mm)+'\n')
+ply.write('comment          output_base    = ' +str(output_base)+'\n')
+ply.write('comment          binned columns = ' +str(num_columns/output_binning)+", binned rows = "+str(num_rows/output_binning)+'\n')
+ply.write('comment \n')
 
 ply.write('element vertex '+str(num_vertices)+'\n')
 ply.write('property float x\n')
@@ -204,8 +233,7 @@ for j in range(rows[0], rows[1], output_binning):
       d = source_coord-vertices[2]
       vertices[6] = vertices[2] + pix[j][i]*d/numpy.linalg.norm(d)
       d = source_coord-vertices[3]
-      vertices[7] = vertices[3] + pix[j][i]*d/numpy.linalg.norm(d)
-
+      vertices[7] = vertices[3] + pix[j][i]*d/numpy.linalg.norm(d)      
 
       # Write vertices coordinates:
       for n in range(0, 8):
@@ -220,8 +248,10 @@ for j in range(rows[0], rows[1], output_binning):
 for n in range(0, num_binned_pixels):
 
   # - Create a triangle mesh combining the walls of the columns of consecutive pixels:
-  ply.write('3 '+str(0+n*8)+' '+str(1+n*8)+' '+str(2+n*8)+'\n')
-  ply.write('3 '+str(2+n*8)+' '+str(3+n*8)+' '+str(0+n*8)+'\n')       #  3 ----- 0   (vertex order clockwise)
+  
+  if (output_base==1):
+    ply.write('3 '+str(0+n*8)+' '+str(1+n*8)+' '+str(2+n*8)+'\n')
+    ply.write('3 '+str(2+n*8)+' '+str(3+n*8)+' '+str(0+n*8)+'\n')     #  3 ----- 0   (vertex order clockwise)    
   ply.write('3 '+str(4+n*8)+' '+str(5+n*8)+' '+str(6+n*8)+'\n')       #    - c -
   ply.write('3 '+str(6+n*8)+' '+str(7+n*8)+' '+str(4+n*8)+'\n')       #  2 ----- 1
   
